d7fde2b36b86351a9743718f285c8bfd837902e9
TFA <thomas.fayolle@cetic.be>
2024-10-16 16:28:52 +0200

    Adding a structure for note and measures

---
+++ src/main/scala/dataStruct/Measure.scala
@@ -0,0 +1,26 @@
+package dataStruct
+
+case class Measure(duration: Int, contents: List[Content]) {
+  require(contents.map(_.duration).sum <= duration)
+  require(List(8,12,16).contains(duration),s"Measure with duration = $duration is not supported yet")
+
+  def timeSig : String = {
+    s"""<TimeSig>
+<sigN>${duration/4}</sigN>
+<sigD>4</sigD>
+</TimeSig>
+"""
+  }
+
+  def toXml(fst : Boolean): String = {
+    val contentsToXml = contents.map(_.toXml).mkString("\n")
+    val time = if (fst) timeSig else ""
+    s"""<Measure>
+<voice>
+$timeSig
+$contentsToXml
+</voice>
+</Measure>
+"""
+  }
+}
