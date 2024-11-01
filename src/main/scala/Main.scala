35e676be342e8a8c631d64f58e222266d7e92990
TFA <thomas.fayolle@cetic.be>
2024-11-01 10:48:10 +0100

    Removing useless lines in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,14 +6,12 @@ import generator.Generator
 
 object Main extends App {
 
-  val measure = Measure(16,List(Note(62,2),Note(64,2),Note(64,4),Rest(4),Note(56,4)))
-
   val out = "Out.mscx"
 
   val scoreGenerator = new Generator(Some(1000))
 
   new PrintWriter(out) {
-    write(printer.Printer.printScore(scoreGenerator.generateScore(4,List(62,64,66,68))))
+    write(printer.Printer.printScore(scoreGenerator.generateScore(4,List(55,57,59,60,62,64,65,67))))
     close()
   }
 
